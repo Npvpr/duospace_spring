05b3a7de8cfc7282a61791f814690b0d32dbd124
Npvpr <nlmnever2@gmail.com>
2024-10-12 22:28:40 +0100

    First commit

---
+++ src/main/java/duospace/duospace_spring/DuospaceSpringApplication.java
@@ -0,0 +1,13 @@
+package duospace.duospace_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuospaceSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuospaceSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/duospace/duospace_spring/DuospaceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package duospace.duospace_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuospaceSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
